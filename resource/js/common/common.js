bac60071f632f01daf2014eb88e93b839fb1281f
jes89 <jungeuisub1989@gmail.com>
2018-10-13 14:11:50 +0900

    커먼 변경

--- resource/js/common/common.js
+++ resource/js/common/common.js
@@ -1,69 +1,53 @@
-var comm = new (function() {
-
-	var headEl = document.getElementsByTagName("head")[0];
-
-	this.getHead = function() {
-		return headEl;
+var singleObj = (function() {
+	var instance;
+	
+	var gObj = {
+			headEl : document.getElementsByTagName("head")[0]
 	}
+	
+	function initiate() {
+		return {
+			getHead : function() {
+				return gObj.headEl;
+			},
+			getNowDate : function(msg) {
+				var now = new Date();
+				var year = now.getFullYear();
+				var month = now.getMonth() + 1;
+				var date = now.getDate();
+				var hour = now.getHours();
+				var minute = now.getMinutes();
+				var second = now.getSeconds();
+				var milliSecond = now.getMilliseconds();
 
-	this.getNowDate = function(msg) {
-		var now = new Date();
-		var year = now.getFullYear();
-		var month = now.getMonth() + 1;
-		var date = now.getDate();
-		var hour = now.getHours();
-		var minute = now.getMinutes();
-		var second = now.getSeconds();
-		var milliSecond = now.getMilliseconds();
+				if (hour < 10) {
+					hour = "0" + hour;
+				}
 
-		if (hour < 10) {
-			hour = "0" + hour;
+				if (second < 10) {
+					second = "0" + second;
+				}
+
+				return year + "-" + month + "-" + date + " " + hour + ":" + minute
+						+ ":" + second + " " + milliSecond;
+			}, 
+			errorLog : function(msg) {
+				var fullDate = getNowDate();
+
+				console.log(fullDate + " : " + msg);
+			}
 		}
+	}
 
-		if (second < 10) {
-			second = "0" + second;
+	return {
+		getInstance : function() {
+			if (!instance) {
+				instance = initiate();
+			}
+			return instance;
 		}
-
-		return year + "-" + month + "-" + date + " " + hour + ":" + minute
-				+ ":" + second + " " + milliSecond;
 	}
-
-	this.errorLog = function(msg) {
-		var fullDate = getNow();
-
-		console.log(fullDate + " : " + msg);
-	}
-
-//	this.required = function(requiredArr) {
-//
-//		if (typeof (requiredArr) !== "object" || !!requiredArr.length == false) {
-//			this.errorLog("required error scriptArr was not Object");
-//			return;
-//		}
-//
-//		var scriptEl = null;
-//		var tempObj = null;
-//		var callbackFn = null;
-//		var scriptUrl = null;
-//		
-//		for (var i = 0, iLen = requiredArr.length; i < iLen; i++) {
-//
-//			tempObj = requiredArr[i];
-//			
-//			scriptUrl = tempObj["scriptUrl"];
-//			callbackFn = tempObj["callbackFn"];
-//			
-//			scriptEl = document.createElement("script");
-//			
-//			scriptEl.type = "text/javascript";
-//			
-//			scriptEl.src = scriptUrl;
-//
-//			headEl.appendChild(scriptEl);
-//			
-//			if(typeof(callbackFn) === "function"){
-//				scriptEl.onload = callbackFn;
-//			}
-//		}
-//	}
 })();
+
+
+var comm = singleObj.getInstance();
