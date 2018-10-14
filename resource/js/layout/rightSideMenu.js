346bbd7588ba761eeae3fe9bf32ccad74d5dd14d
jes89 <jungeuisub1989@gmail.com>
2018-10-14 14:31:01 +0900

    오른쪽 메뉴에 접기펴기 기능 추가

--- resource/js/layout/rightSideMenu.js
+++ resource/js/layout/rightSideMenu.js
@@ -1,25 +1,55 @@
 (function(){
 	var UserRightSideMene = function() {
 		
-		
-		
 		this.init = function(userNm) {
 			initLayout();
 		}
-			
+		
 		var initLayout = function(){
 			var rightSideMenuContainer = document.createElement("div");
+			var arrowWrapper = document.createElement("div");
+			var arrowBtn = document.createElement("i");
 			var rootContainer = comm.getElById("rootContainer");
 			
+			arrowWrapper.className = "right_menu_arrow";
 			rightSideMenuContainer.className = "right_menu";
-
+			
+			arrowWrapper.appendChild(arrowBtn);
+			rightSideMenuContainer.appendChild(arrowWrapper);
 			rootContainer.appendChild(rightSideMenuContainer);
+			
+			initProperties(rightSideMenuContainer,arrowWrapper ,arrowBtn);
+			
+			initEvent();
 		}
 		
-//		<div style="width: 25px;height: 25px;background-color: white;position: absolute;left: -100px;">
-//		<img src="/img/iconmonstr-arrow-27.svg">
-//		</div>
+		var initProperties = function(rightSideMenuContainer, arrowWrapper, arrowBtn){
+			this.rightSideMenuContainer = rightSideMenuContainer;
+			this.arrowWrapper = arrowWrapper;
+			this.arrowBtn = arrowBtn;
+		}
 		
+		var initEvent = function(){
+			
+			var rightSideMenuContainer = this.rightSideMenuContainer;
+			var arrowWrapper = this.arrowWrapper;
+			var arrowBtn = this.arrowBtn;
+			
+			arrowBtn.addEventListener("click", function(){
+				
+				var classList = this.classList;
+				
+				if(classList.contains("active")){
+					rightSideMenuContainer.style.width = "0";
+					arrowWrapper.style.left = "-25px";
+					classList.remove("active");
+				} else{
+					rightSideMenuContainer.style.width = "300px";
+					arrowWrapper.style.left = "-35px";
+					classList.add("active");
+				}
+			});
+		}
 	}
 
     var rightSideMene = new UserRightSideMene();
