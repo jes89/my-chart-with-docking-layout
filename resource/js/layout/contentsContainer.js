172facc6db8fc2a290c107fd4b3c3f9158cdbf24
jes89 <jungeuisub1989@gmail.com>
2018-10-14 12:42:25 +0900

    콘텐츠 컨테이너 레이아웃 작업

--- resource/js/layout/contentsContainer.js
+++ resource/js/layout/contentsContainer.js
@@ -1,22 +1,40 @@
 var ContentsContainer = function() {
 	
-	
-	
-	this.init = function(userNm) {
-		initLayout();
+	this.init = function(subContainerSize) {
+		initLayout(subContainerSize);
 	}
 		
-	var initLayout = function(){
+	var initLayout = function(subContainerSize){
 		var contentsWrapper = document.createElement("div");
+	
 		var rootContainer = comm.getElById("rootContainer");
 		
-		contentsWrapper.className = "contents_container";
-
+		contentsWrapper.className = "contents_root_container";
+		
 		rootContainer.appendChild(contentsWrapper);
+		
+		initSubContainer(contentsWrapper, subContainerSize);
+		
 	}
 	
+	var initSubContainer = function(contentsWrapper, subContainerSize){
+		
+		var border = 1 * 2;
+		var wrapperhHeight = contentsWrapper.offsetHeight;
+		var eachHeight = (wrapperhHeight / subContainerSize) - border;
+		var tempEl = null;
+		
+		for(var ix = 0; ix < subContainerSize; ix ++){
+			
+			tempEl = document.createElement("div");
+			
+			tempEl.className = "contents_sub_container";
+			
+			tempEl.style.height = eachHeight + "px";
+			
+			contentsWrapper.appendChild(tempEl);
+		
+		}
+	}
 }
 
-var contentsContainer = new ContentsContainer();
-
-contentsContainer.init();
